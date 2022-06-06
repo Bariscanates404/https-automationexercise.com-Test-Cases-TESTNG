ccd9a9634e2246f21b18ae15495a307c7a60efb4
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-07 02:19:12 +0300

    AutomationEx_07.java
     AutomationEx_07.java

---
+++ AutomationEx_07.java
@@ -0,0 +1,53 @@
+package tests.automationExCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AutomationExPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AutomationEx_07 extends TestBaseRapor {
+
+    AutomationExPage automationExPage;
+
+    @Test
+    public void test02_Login_User_With_Correct_Info() throws InterruptedException {
+        automationExPage = new AutomationExPage();
+        extentTest = extentReports.createTest("test02_Login_User_With_Correct_Info", "automationexercise.com UI testcase 2");
+        extentTest.info("1. Launch browser, 2. Navigate to url 'http://automationexercise.com");
+        Driver.getDriver().get(ConfigReader.getProperty("autExUrl"));
+        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        extentTest.info("3. Verify that home page is visible successfully");
+        String actualURL = Driver.getDriver().getCurrentUrl();
+        String expectedURL = "https://automationexercise.com/";
+        Assert.assertEquals(actualURL, expectedURL);
+        List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
+        for (WebElement element : mainPageTumResimler) {
+            element.isDisplayed();
+            element.isEnabled();
+        }
+        extentTest.info("4. Click on 'Test Cases' button");
+        List<WebElement> mainPageTestCasesButtons = Driver.getDriver().findElements(By.xpath("//button[text()='Test Cases']"));
+        for (WebElement button : mainPageTestCasesButtons) {
+            try
+            {
+                if(button.isDisplayed())
+                {
+                    button.click();
+                }
+            }
+            catch(Exception e){}
+        }
+        extentTest.info("5. Verify user is navigated to test cases page successfully");
+        String actualTestCasesURL = Driver.getDriver().getCurrentUrl();
+        String expectedTestCasesURL = "https://automationexercise.com/test_cases";
+        Assert.assertEquals(actualTestCasesURL, expectedTestCasesURL);
+        extentTest.pass("Test cases button working expectedly!");
+    }
+}
