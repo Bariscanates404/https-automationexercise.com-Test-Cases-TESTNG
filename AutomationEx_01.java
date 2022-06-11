24429ae102c32f2b5cdd0ff346c411d5e824aa45
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-11 22:13:25 +0300

    AutomationEx_01.java
    AutomationEx_01.java

--- AutomationEx_01.java
+++ AutomationEx_01.java
@@ -1,6 +1,7 @@
 package tests.automationExCases;
 
 import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -11,6 +12,7 @@ import utilities.Driver;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
+import java.util.List;
 
 public class AutomationEx_01 extends TestBaseRapor {
     AutomationExPage automationExPage;
@@ -20,6 +22,9 @@ public class AutomationEx_01 extends TestBaseRapor {
     public void test01_Register_User() throws InterruptedException {
         automationExPage = new AutomationExPage();
         Faker faker = new Faker();
+        String fakeFirstName = faker.name().nameWithMiddle();
+        String fakeEmail = faker.internet().emailAddress();
+        String fakePassword = faker.internet().password();
         String fakeAddress = faker.address().streetAddressNumber();
         String fakeAddress2 = faker.address().fullAddress();
         String fakeMobilePhone = faker.phoneNumber().cellPhone();
@@ -32,26 +37,27 @@ public class AutomationEx_01 extends TestBaseRapor {
         String actualURL = Driver.getDriver().getCurrentUrl();
         String expectedURL = "https://automationexercise.com/";
         Assert.assertEquals(actualURL, expectedURL);
-        for (WebElement element : automationExPage.sayfadakiTumResimler) {
+        List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
+        for (WebElement element : mainPageTumResimler) {
             element.isDisplayed();
             element.isEnabled();
         }
         extentTest.info("4. Click on 'Signup / Login' button");
-        automationExPage.loginButonu.click();
+        automationExPage.mainPageloginSingupButonu.click();
         extentTest.info("5. Verify 'New User Signup!' is visible");
-        automationExPage.newUserSingupMenu.isDisplayed();
+        automationExPage.newUserSingupMenuForm.isDisplayed();
         extentTest.info("6. Enter name and email address");
-        automationExPage.newUserSingupMenuName.sendKeys(ConfigReader.getProperty("autExValidName"));
-        automationExPage.newUserSingupMenuEmail.sendKeys(ConfigReader.getProperty("autExEmail"));
-        extentTest.info("//7. Click 'Signup' button ");
+        automationExPage.newUserSingupMenuName.sendKeys(fakeFirstName);
+        automationExPage.newUserSingupMenuEmail.sendKeys(fakeEmail);
+        extentTest.info("7. Click 'Signup' button ");
         automationExPage.newUserSingupButton.click();
-        extentTest.info("//8. Verify that 'ENTER ACCOUNT INFORMATION' is visible");
+        extentTest.info("8. Verify that 'ENTER ACCOUNT INFORMATION' is visible");
         automationExPage.loginForm.isDisplayed();
-        extentTest.info("//9. Fill details: Title, Name, Email, Password, Date of birth");
-        if (!automationExPage.titleRadioButtonGenderMr.isSelected()) {
-            automationExPage.titleRadioButtonGenderMr.click();
+        extentTest.info("9. Fill details: Title, Name, Email, Password, Date of birth");
+        if (!automationExPage.loginFormtitleRadioButtonGenderMr.isSelected()) {
+            automationExPage.loginFormtitleRadioButtonGenderMr.click();
         }
-        automationExPage.loginFormPassword.sendKeys(ConfigReader.getProperty("autExValidPassword"));
+        automationExPage.loginFormPassword.sendKeys(fakePassword);
         select = new Select(automationExPage.loginFormDropdownDay);
         select.selectByVisibleText("1");
         select = new Select(automationExPage.loginFormDropdownMonth);
@@ -74,7 +80,7 @@ public class AutomationEx_01 extends TestBaseRapor {
         automationExPage.loginFormAdressInfoZipcode.sendKeys("02108");
         automationExPage.loginFormAdressInfoMobileNumber.sendKeys(fakeMobilePhone);
         extentTest.info("13. Click 'Create Account button' ");
-        automationExPage.loginFormAdressInfoCreateAccount.click();
+        automationExPage.loginFormAdressInfoCreateAccountButton.click();
         extentTest.info("14. Verify that 'ACCOUNT CREATED!' is visible ");
         String actualText = automationExPage.verifyAccountCreated.getText();
         String expectedText = "ACCOUNT CREATED!";
@@ -83,8 +89,8 @@ public class AutomationEx_01 extends TestBaseRapor {
         automationExPage.accountCreateContinueButton.click();
         extentTest.info("16. Verify that 'Logged in as username' is visible ");
         String actualId = automationExPage.loggedUserName.getText();
-        String expectedId = ConfigReader.getProperty("autExValidName");
+        String expectedId = fakeFirstName;
         Assert.assertEquals(actualId, expectedId);
         extentTest.pass("User logged in succesfully!");
-    } 
+    }
 }
