9f4456cbee62b86256d24a1f0e4dc7fc263c190f
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-11 22:13:43 +0300

    Update AutomationEx_02.java

--- AutomationEx_02.java
+++ AutomationEx_02.java
@@ -1,5 +1,6 @@
 package tests.automationExCases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ import utilities.Driver;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
+import java.util.List;
 
 public class AutomationEx_02 extends TestBaseRapor {
 
@@ -17,7 +19,7 @@ public class AutomationEx_02 extends TestBaseRapor {
     @Test
     public void test02_Login_User_With_Correct_Info() throws InterruptedException {
         automationExPage = new AutomationExPage();
-        extentTest = extentReports.createTest("test0Login_User_With_Correct_Info", "automationexercise.com UI test case 2");
+        extentTest = extentReports.createTest("test02_Login_User_With_Correct_Info", "automationexercise.com UI testcase 2");
         extentTest.info("1. Launch browser, 2. Navigate to url 'http://automationexercise.com");
         Driver.getDriver().get(ConfigReader.getProperty("autExUrl"));
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -25,12 +27,13 @@ public class AutomationEx_02 extends TestBaseRapor {
         String actualURL = Driver.getDriver().getCurrentUrl();
         String expectedURL = "https://automationexercise.com/";
         Assert.assertEquals(actualURL, expectedURL);
-        for (WebElement element : automationExPage.sayfadakiTumResimler) {
+        List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
+        for (WebElement element : mainPageTumResimler) {
             element.isDisplayed();
             element.isEnabled();
         }
         extentTest.info("4. Click on 'Signup / Login' button");
-        automationExPage.loginSingupButonu.click();
+        automationExPage.mainPageloginSingupButonu.click();
         extentTest.info("5. Verify 'Login to your account' is visible");
         automationExPage.loginUserMenuForm.isDisplayed();
         extentTest.info("6. Enter correct email address and password");
