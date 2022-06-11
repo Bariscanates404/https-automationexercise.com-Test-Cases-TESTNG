eb5c4b8db9dc730dc102337189bbc54033595cb6
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-12 00:37:38 +0300

    AutomationEx_01.java
    AutomationEx_01.java

--- AutomationEx_01.java
+++ AutomationEx_01.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import pages.AutomationExPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
@@ -32,11 +33,9 @@ public class AutomationEx_01 extends TestBaseRapor {
         extentTest = extentReports.createTest("test01_Register_User", "automationexercise.com UI testcase 1");
         extentTest.info("1. Launch browser, 2. Navigate to url 'http://automationexercise.com");
         Driver.getDriver().get(ConfigReader.getProperty("autExUrl"));
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ReusableMethods.waitForPageToLoad(10);
         extentTest.info("3. Verify that home page is visible successfully");
-        String actualURL = Driver.getDriver().getCurrentUrl();
-        String expectedURL = "https://automationexercise.com/";
-        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertTrue(ReusableMethods.verifyURLorText(Driver.getDriver().getCurrentUrl(), "https://automationexercise.com/"));
         List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
         for (WebElement element : mainPageTumResimler) {
             element.isDisplayed();
