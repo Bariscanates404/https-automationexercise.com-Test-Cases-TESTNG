e2d104a4e716721d6530d44a451e08af1bb297f4
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-12 00:40:26 +0300

    AutomationEx_07.java
    AutomationEx_07.java

--- AutomationEx_07.java
+++ AutomationEx_07.java
@@ -7,13 +7,13 @@ import org.testng.annotations.Test;
 import pages.AutomationExPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
 import java.util.List;
 
 public class AutomationEx_07 extends TestBaseRapor {
-
     AutomationExPage automationExPage;
 
     @Test
@@ -22,11 +22,9 @@ public class AutomationEx_07 extends TestBaseRapor {
         extentTest = extentReports.createTest("test07_Verify_Testcases_Page", "automationexercise.com UI testcase 7");
         extentTest.info("1. Launch browser, 2. Navigate to url 'http://automationexercise.com");
         Driver.getDriver().get(ConfigReader.getProperty("autExUrl"));
-        Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        ReusableMethods.waitForPageToLoad(10);
         extentTest.info("3. Verify that home page is visible successfully");
-        String actualURL = Driver.getDriver().getCurrentUrl();
-        String expectedURL = "https://automationexercise.com/";
-        Assert.assertEquals(actualURL, expectedURL);
+        Assert.assertTrue(ReusableMethods.verifyURLorText(Driver.getDriver().getCurrentUrl(), "https://automationexercise.com/"));
         List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
         for (WebElement element : mainPageTumResimler) {
             element.isDisplayed();
