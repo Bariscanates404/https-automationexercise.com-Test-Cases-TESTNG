e477f95942b305c5bfd71c34a085881fa70ea6d1
Baris Can Ateş <103248330+Bariscanates404@users.noreply.github.com>
2022-06-11 22:15:11 +0300

    AutomationEx_05.java
    AutomationEx_05.java

--- AutomationEx_05.java
+++ AutomationEx_05.java
@@ -1,5 +1,6 @@
 package tests.automationExCases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,6 +10,7 @@ import utilities.Driver;
 import utilities.TestBaseRapor;
 
 import java.time.Duration;
+import java.util.List;
 
 public class AutomationEx_05 extends TestBaseRapor {
 
@@ -25,12 +27,13 @@ public class AutomationEx_05 extends TestBaseRapor {
         String actualURL = Driver.getDriver().getCurrentUrl();
         String expectedURL = "https://automationexercise.com/";
         Assert.assertEquals(actualURL, expectedURL);
-        for (WebElement element : automationExPage.sayfadakiTumResimler) {
+        List<WebElement> mainPageTumResimler = Driver.getDriver().findElements(By.xpath("//div[@class='productinfo text-center']"));
+        for (WebElement element : mainPageTumResimler) {
             element.isDisplayed();
             element.isEnabled();
         }
         extentTest.info("4. Click on 'Signup / Login' button");
-        automationExPage.loginSingupButonu.click();
+        automationExPage.mainPageloginSingupButonu.click();
         extentTest.info("5. Verify 'New User Signup!' is visible");
         automationExPage.newUserSingupMenuForm.isDisplayed();
         extentTest.info("6. Enter name and already registered email address");
